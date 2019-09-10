7e54b145d7e4307df2444bc887e58ca2aea34e80
Ryuichi Ueda <ryuichiueda@gmail.com>
2019-09-10 18:23:04 +0900

    First commit

---
+++ oneliner-python.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class Opy < Formula
+  homepage "https://github.com/ryuichiueda/opy"
+  url "https://github.com/ryuichiueda/opy/archive/v1.4.4.tar.gz"
+  sha256 "f3ab839ff0e919bcf5764fb0a8eb69904d3eba2fa3eb4f19ebf60c464a782a73"
+  head "https://github.com/ryuichiueda/opy.git"
+  version "1.4.4"
+
+  def install
+    bin.install "opy"
+    man1.install "opy.1" 
+  end
+end
