db43422e30fea388998b0556ee7b6dcc0d47f782
Ryuichi Ueda <ryuichiueda@gmail.com>
2019-09-29 15:34:38 +0900

    Update to 1.9.1

--- oneliner-python.rb
+++ oneliner-python.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class OnelinerPython < Formula
   homepage "https://github.com/ryuichiueda/opy"
-  url "https://github.com/ryuichiueda/opy/archive/v1.8.0.tar.gz"
-  sha256 "4d94c619635f2d3bf58c1e2fc4017386169c6f6ef6d1427e722092d873e181b9"
+  url "https://github.com/ryuichiueda/opy/archive/v1.9.1.tar.gz"
+  sha256 "74ecf29638e8f347f636f36129522a20cb9129acd9fa8066e4cec964b2b0c85b"
   head "https://github.com/ryuichiueda/opy.git"
-  version "1.8.0"
+  version "1.9.1"
 
   def install
     bin.install "opy"
