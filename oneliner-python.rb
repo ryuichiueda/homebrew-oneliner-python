172fed6d0abd013da6bc7c944aa357a3a47fc0bf
Ryuichi Ueda <ryuichiueda@gmail.com>
2019-10-08 23:02:23 +0900

    Fix

--- oneliner-python.rb
+++ oneliner-python.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class OnelinerPython < Formula
   homepage "https://github.com/ryuichiueda/opy"
-  url "https://github.com/ryuichiueda/opy/archive/v1.9.3tar.gz"
+  url "https://github.com/ryuichiueda/opy/archive/v1.9.3.tar.gz"
   sha256 "2424377ce624ad1ca04f335fb06677dd67999664e534ff61159653f46afa8db4"
   head "https://github.com/ryuichiueda/opy.git"
   version "1.9.3"
